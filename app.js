3c7a238cab021681cb8c933a24cdd1626a93a8fc
Arquez_Mc <00029624@uca.edu.sv>
2026-09-15 07:41:38 -0600

    feat: completar endpoints de estadisticas y clasificacion automatica

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const incidenciasRoutes = require('./routes/incidenciasRoutes');
+const incidenciasRoutes = require('./routes/incidencias');
 
 const app = express();
 const port = 3124;
